a23913fd1cec9669800f1d1cceaf932b034737c9
Hanaasagi <ambiguous404@gmail.com>
2023-03-25 19:37:51 +0800

    @date03/25/2023@now19:03

---
+++ src/starlark-parser/tokenize/position.ts
@@ -0,0 +1,56 @@
+// A Position dethisribes the location of a rune of input.
+export class Position {
+  file: string | null; // filename (indirect for compactness)
+  line: number; // 1-based line number; 0 if line unknown
+  col: number; // 1-based column (rune) number; 0 if column unknown
+
+  // MakePosition returns position with the specified components.
+  constructor(file: string | null, line: number, col: number) {
+    this.file = file;
+    this.line = line;
+    this.col = col;
+  }
+
+  // IsValid reports whether the position is valid.
+  isValid(): boolean {
+    return this.file !== null;
+  }
+
+  // Filename returns the name of the file containing this position.
+  filename(): string {
+    if (this.file !== null) {
+      return this.file;
+    }
+    return "<invalid>";
+  }
+
+  // add returns the position at the end of s, assuming it starts at p.
+  add(s: string): Position {
+    if (s.includes("\n")) {
+      const n = s.split("\n").length - 1;
+      this.line += n;
+      s = s.substring(s.lastIndexOf("\n")! + 1, s.length);
+      this.col = 1;
+    }
+    this.col += s.length;
+    return this;
+  }
+
+  toString(): string {
+    const file = this.filename();
+    if (this.line > 0) {
+      if (this.col > 0) {
+        return `${file}:${this.line}:${this.col}`;
+      }
+      return `${file}:${this.line}`;
+    }
+    return file;
+  }
+
+  isBefore(q: Position): boolean {
+    if (this.line !== q.line) {
+      return this.line < q.line;
+    }
+    return this.col < q.col;
+  }
+}
---
+++ src/starlark-parser/tokenize/token.ts
@@ -0,0 +1,195 @@
+import { Position } from "./position";
+
+// A Token represents a Starlark lexical token.
+export enum Token {
+  // illegal token
+  ILLEGAL = "illegal token",
+  // end of file
+  EOF = "end of file",
+  // newline
+  NEWLINE = "newline",
+  // indent
+  INDENT = "indent",
+  // outdent
+  OUTDENT = "outdent",
+  // identifier
+  IDENT = "identifier",
+  // int literal
+  INT = "int literal",
+  // float literal
+  FLOAT = "float literal",
+  // string literal
+  STRING = "string literal",
+  // bytes literal
+  BYTES = "bytes literal",
+  // +
+  PLUS = "+",
+  // -
+  MINUS = "-",
+  // *
+  STAR = "*",
+  // /
+  SLASH = "/",
+  // //
+  SLASHSLASH = "//",
+  // %
+  PERCENT = "%",
+  // &
+  AMP = "&",
+  // |
+  PIPE = "|",
+  // ^
+  CIRCUMFLEX = "^",
+  // <<
+  LTLT = "<<",
+  // >>
+  GTGT = ">>",
+  // ~
+  TILDE = "~",
+  // .
+  DOT = ".",
+  //,
+  COMMA = ",",
+  // =
+  EQ = "=",
+  // ;
+  SEMI = ";",
+  // :
+  COLON = ":",
+  // (
+  LPAREN = "(",
+  // )
+  RPAREN = ")",
+  // [
+  LBRACK = "[",
+  // ]
+  RBRACK = "]",
+  // {
+  LBRACE = "{",
+  // }
+  RBRACE = "}",
+  // <
+  LT = "<",
+  // >
+  GT = ">",
+  // >=
+  GE = ">=",
+  // <=
+  LE = "<=",
+  // ==
+  EQL = "==",
+  // !=
+  NEQ = "!=",
+  // +=
+  PLUS_EQ = "+=",
+  // -=
+  MINUS_EQ = "-=",
+  // *=
+  STAR_EQ = "*=",
+  // /=
+  SLASH_EQ = "/=",
+  // //=
+  SLASHSLASH_EQ = "//=",
+  // %=
+  PERCENT_EQ = "%=",
+  // &=
+  AMP_EQ = "&=",
+  // |=
+  PIPE_EQ = "|=",
+  // ^=
+  CIRCUMFLEX_EQ = "^=",
+  // <<=
+  LTLT_EQ = "<<=",
+  // >>=
+  GTGT_EQ = ">>=",
+  // **
+  STARSTAR = "**",
+  // and
+  AND = "and",
+  // break
+  BREAK = "break",
+  // continue
+  CONTINUE = "continue",
+  // def
+  DEF = "def",
+  // elif
+  ELIF = "elif",
+  // else
+  ELSE = "else",
+  // for
+  FOR = "for",
+  // if
+  IF = "if",
+  // in
+  IN = "in",
+  // lambda
+  LAMBDA = "lambda",
+  // load
+  LOAD = "load",
+  // not
+  NOT = "not",
+  // not in (synthesized by parser)
+  NOT_IN = "not in",
+  // or
+  OR = "or",
+  // pass
+  PASS = "pass",
+  // return
+  RETURN = "return",
+  // while
+  WHILE = "while",
+}
+
+// BUG:
+export const keywordToken: Record<string, Token> = {
+  and: Token.AND,
+  break: Token.BREAK,
+  continue: Token.CONTINUE,
+  def: Token.DEF,
+  elif: Token.ELIF,
+  else: Token.ELSE,
+  for: Token.FOR,
+  if: Token.IF,
+  in: Token.IN,
+  lambda: Token.LAMBDA,
+  load: Token.LOAD,
+  not: Token.NOT,
+  or: Token.OR,
+  pass: Token.PASS,
+  return: Token.RETURN,
+  while: Token.WHILE,
+
+  // reserved words:
+  as: Token.ILLEGAL,
+  // "assert":   ILLEGAL, // heavily used by our tests
+  class: Token.ILLEGAL,
+  del: Token.ILLEGAL,
+  except: Token.ILLEGAL,
+  finally: Token.ILLEGAL,
+  from: Token.ILLEGAL,
+  global: Token.ILLEGAL,
+  import: Token.ILLEGAL,
+  is: Token.ILLEGAL,
+  nonlocal: Token.ILLEGAL,
+  raise: Token.ILLEGAL,
+  try: Token.ILLEGAL,
+  with: Token.ILLEGAL,
+  yield: Token.ILLEGAL,
+};
+
+// tokenValue records the position and value associated with each token.
+// @ts-ignore
+export class TokenValue {
+  // @ts-ignore
+  raw: string; // raw text of token
+  // @ts-ignore
+  int: number; // decoded int
+  // @ts-ignore
+  bigInt: bigint | null; // decoded integers > int64
+  // @ts-ignore
+  float: number; // decoded float
+  // @ts-ignore
+  string: string; // decoded string or bytes
+  // @ts-ignore
+  pos: Position; // start position of token
+}
